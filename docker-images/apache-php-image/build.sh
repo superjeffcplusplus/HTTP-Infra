28c8300bccc731c1d46907f4fac8610738be1fd2
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2021-12-16 08:29:35 +0100

    part 1

--- docker-images/apache-php-image/build.sh
+++ docker-images/apache-php-image/build.sh
@@ -1,10 +1,8 @@
+#!/bin/bash
+
+# This script kills the php container if running and rebuilds it
 docker kill http-infra 2>/dev/null;
 docker rm http-infra 2>/dev/null;
 docker image rm api/http-infra 2>/dev/null;
 
 docker build -t api/http-infra . ;
-
-docker run -d \
-        -p 80:80 \
-        --name http-infra \
-        api/http-infra
