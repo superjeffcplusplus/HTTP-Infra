e0fa3cb9e2aaee924dad8050649309124b06806c
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2021-12-16 12:31:48 +0100

    part 3 completed

--- docker-images/reverse-proxy-apache-httpd/run-all.sh
+++ docker-images/reverse-proxy-apache-httpd/run-all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker run -d --name php_static api/php;
-docker run -d --name apache2_rp api/apache2_rp;
+docker run -d --name http_dynamic api/http-dynamic;
+docker run -d -p 8080:80 --name apache_rp api/apache_rp;
