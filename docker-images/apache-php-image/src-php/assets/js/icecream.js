ce4ce1846e1a9af39e6d9dc9ddbba60d1cb6938e
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-07 18:12:27 +0100

    dynamic content ok

--- docker-images/apache-php-image/src-php/assets/js/icecream.js
+++ docker-images/apache-php-image/src-php/assets/js/icecream.js
@@ -1,15 +1,21 @@
 $(
     function () {
-        console.log("loading icecreams");
+        console.log("loading ice creams");
 
         function loadIcecream() {
             $.getJSON("/api/icecream/", (icecreams) => {
-                console.log(icecreams);
-                
+                $(".dynamic-content").empty();
+                content = "<ul class='w3-black'><p >" + icecreams.iceCreamBowls.length + " bowls : </p>";
+                icecreams.iceCreamBowls.forEach(element => {
+                    content += "<li>" + element + "</li>";
+                });
+                content += "</ul>";
+                $(".dynamic-content").append(content);
             });
             
         }
 
         loadIcecream();
+        setInterval(loadIcecream, 2000);
     }
 )
