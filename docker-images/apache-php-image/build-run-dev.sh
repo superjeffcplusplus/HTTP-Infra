2bb2069f85d9f04fba944bf7ec5a71f9a300f7c6
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-18 20:50:59 +0100

    Scripts docker testés

--- docker-images/apache-php-image/build-run-dev.sh
+++ docker-images/apache-php-image/build-run-dev.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# arrête le container 
+docker container kill api-web-static-dev;
+# supprime le container
 docker container rm api-web-static-dev 2>/dev/null;
 docker build -t api/apache-static-dev . ;
 docker run -d \
-        -p 80:80 \
+        # lie le port 8080 de l'hôte au port 80 du container
+        -p 80:8080 \ 
         --name api-web-static-dev \
+        # Volume
         --mount type=bind,src=$(pwd)/src-php,target=/var/www/html/ \
         api/apache-static-dev ;
--- docker-images/apache-php-image/build-run-production.sh
+++ docker-images/apache-php-image/build-run-production.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+docker kill api-web-static 2>/dev/null; 
 docker container rm api-web-static 2>/dev/null;
 docker build -t api/apache-static . ;
 docker run -d \
-        -p 80:80 \
+        -p 80:8080 \
         --name api-web-static \
         api/apache-static ;
