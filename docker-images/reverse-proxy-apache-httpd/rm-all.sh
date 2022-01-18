41a95d1ba39e5de0d2cb1613b47d3a0bfebecbb5
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-18 20:53:48 +0100

    suppression scripts Docker

--- docker-images/reverse-proxy-apache-httpd/rm-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker container rm web_static 2>/dev/null;
-docker container rm web_dynamic 2>/dev/null;
-docker container rm apache_rp 2>/dev/null;
--- docker-images/reverse-proxy-apache-httpd/run-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker run -d --name web_static --hostname web_static api/php;
-docker run -d --name web_dynamic --hostname web_dynamic api/http-dynamic;
-docker run -d -p 8080:80 --name apache_rp api/apache_rp;
