87b2a7734e2113cdc3bdf20a670bc0f006556250
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2022-01-19 19:41:34 +0100

    grand nettoyage

--- docker-images/apache-php-image/build-run-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker container rm api-web-static-dev 2>/dev/null;
-docker build -t api/apache-static-dev . ;
-docker run -d \
-        -p 80:80 \
-        --name api-web-static-dev \
-        --mount type=bind,src=$(pwd)/src-php,target=/var/www/html/ \
-        api/apache-static-dev ;
--- docker-images/reverse-proxy-apache-httpd/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script kills the php container if running and rebuilds it
-docker kill api/apache_rp 2>/dev/null;
-docker rm api/apache_rp 2>/dev/null;
-docker image rm api/apache_rp 2>/dev/null;
-
-docker build -t api/apache_rp . ;
